4dfce63536ff3fcdd31573c08d48711f947efb7e
Chris Ferdinandi <cferdinandi@users.noreply.github.com>
2017-08-25 10:55:16 -0400

    Create matches-and-closest.js

---
+++ polyfills/matches-and-closest.js
@@ -0,0 +1,33 @@
+/**
+ * Element.matches() and Element.closest() polyfills
+ * https://developer.mozilla.org/en-US/docs/Web/API/Element/matches#Polyfill
+ * https://developer.mozilla.org/en-US/docs/Web/API/Element/closest#Polyfill
+ */
+
+if (!Element.prototype.matches) {
+	Element.prototype.matches =
+		Element.prototype.matchesSelector ||
+		Element.prototype.mozMatchesSelector ||
+		Element.prototype.msMatchesSelector ||
+		Element.prototype.oMatchesSelector ||
+		Element.prototype.webkitMatchesSelector ||
+		function (s) {
+			var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+				i = matches.length;
+			while (--i >= 0 && matches.item(i) !== this) { }
+			return i > -1;
+		};
+}
+
+if (!Element.prototype.closest) {
+	Element.prototype.closest = function (s) {
+		var el = this;
+		var ancestor = this;
+		if (!document.documentElement.contains(el)) return null;
+		do {
+			if (ancestor.matches(s)) return ancestor;
+			ancestor = ancestor.parentElement;
+		} while (ancestor !== null);
+		return null;
+	};
+}
