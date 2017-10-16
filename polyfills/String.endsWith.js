f18c3d89ecea9699484c059962275803a00ce2e1
Chris Ferdinandi <cferdinandi@users.noreply.github.com>
2017-10-16 13:53:23 -0400

    Create String.endsWith.js

---
+++ polyfills/String.endsWith.js
@@ -0,0 +1,16 @@
+/**
+ * String.prototype.endsWith() polyfill
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith#Polyfill
+ */
+if (!String.prototype.endsWith) {
+	String.prototype.endsWith = function(searchStr, Position) {
+		// This works much better than >= because
+		// it compensates for NaN:
+		if (!(Position < this.length)) {
+			Position = this.length;
+		} else {
+			Position |= 0; // round position
+		}
+		return this.substr(Position - searchStr.length, searchStr.length) === searchStr;
+	};
+}
