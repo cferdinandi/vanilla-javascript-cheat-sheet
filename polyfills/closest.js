4d84cc8deb149a0510e63e900212b86e5c36dc94
Chris Ferdinandi <cferdinandi@users.noreply.github.com>
2017-10-17 15:28:28 -0400

    Update closest.js

--- polyfills/closest.js
+++ polyfills/closest.js
@@ -1,9 +1,11 @@
 /**
  * Element.closest() polyfill
  * https://developer.mozilla.org/en-US/docs/Web/API/Element/closest#Polyfill
- * NOTE: Requires the matches() polyfill - https://github.com/cferdinandi/vanilla-javascript-cheat-sheet/blob/master/polyfills/matches.js
  */
 if (!Element.prototype.closest) {
+	if (!Element.prototype.matches) {
+		Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+	}
 	Element.prototype.closest = function (s) {
 		var el = this;
 		var ancestor = this;
