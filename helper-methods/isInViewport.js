2d3282da9d55c5fd59e4dab82775f33f31019b55
Chris Ferdinandi <cferdinandi@users.noreply.github.com>
2017-10-17 15:42:03 -0400

    Create isInViewport.js

---
+++ helper-methods/isInViewport.js
@@ -0,0 +1,14 @@
+/**
+ * Determine if an element is in the viewport
+ * @param  {Node}    elem The element
+ * @return {Boolean}      Returns true if element is in the viewport
+ */
+var isInViewport = function (elem) {
+	var distance = elem.getBoundingClientRect();
+	return (
+		distance.top >= 0 &&
+		distance.left >= 0 &&
+		distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+		distance.right <= (window.innerWidth || document.documentElement.clientWidth)
+	);
+};
