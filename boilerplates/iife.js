a1dc74a8253db7a2a52d880a079cf38ab5c37817
Chris Ferdinandi <cferdinandi@users.noreply.github.com>
2017-10-23 13:59:03 -0400

    Create iife.js

---
+++ boilerplates/iife.js
@@ -0,0 +1,11 @@
+/*!
+ * Immediately Invoked Function Expression Boilerplate
+ * (c) 2017 Chris Ferdinandi, MIT License, https://gomakethings.com
+ */
+;(function (window, document, undefined) {
+
+    'use strict';
+
+    // Code goes here...
+
+})(window, document);
