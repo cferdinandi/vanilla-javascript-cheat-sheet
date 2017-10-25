b84fe5e8593e67f4ddde1f2cb8192503bac89420
Chris Ferdinandi <cferdinandi@users.noreply.github.com>
2017-10-25 14:40:43 -0400

    Update Object.forEach.js

--- polyfills/Object.forEach.js
+++ polyfills/Object.forEach.js
@@ -7,6 +7,9 @@
 if (!Object.prototype.forEach) {
 	Object.defineProperty(Object.prototype, 'forEach', {
 		value: function (callback, thisArg) {
+			if (this == null) {
+				throw new TypeError('Not an object');
+			}
 			thisArg = thisArg || window;
 			for (var key in this) {
 				if (this.hasOwnProperty(key)) {
